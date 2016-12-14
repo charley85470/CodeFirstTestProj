f73d9425028ece1667c9129d277f3d7085ebd557
charleychang@86shop.com.tw <charleychang@86shop.com.tw>
2016-12-14 13:58:00 +0800

    Program.cs modified

--- CodeFirstTestProj/Program.cs
+++ CodeFirstTestProj/Program.cs
@@ -14,16 +14,21 @@ namespace CodeFirstTestProj
         {
             using (var db = new CompanyContext())
             {
+				// 輸入部門名稱
                 Console.Write("Insert Department Name:");
                 var depName = Console.ReadLine();
 
+				// 建立部門物件
                 var dep = new Department { DepartmentId = Guid.NewGuid(), DepartmentName = depName };
+				// 將部門物件輸入至資料庫
                 db.Departments.Add(dep);
                 db.SaveChanges();
 
+				// 執行Linq查詢(T-SQL:SELECT * FROM Departments)
                 var qry = from d in db.Departments
                           select d;
 
+				// Print Department
                 foreach (var item in qry)
                 {
                     Console.WriteLine($@"ID = {item.DepartmentId} , Name = {item.DepartmentName}");
